e4a1fd8a0f7d5e3f7ff1245361661c566150549d
Matan Kulikovsky <matan@soom.la>
2014-07-22 15:07:34 +0300

    changed grunt tasks order

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,5 +6,5 @@ module.exports = function(grunt) {
 
   var gruntConfig = require('./grunt-config.json');
   grunt.initConfig(gruntConfig);
-  grunt.registerTask('default', ['uglify', 'concat', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['copy', 'uglify', 'concat', 'cssmin']);
 };
