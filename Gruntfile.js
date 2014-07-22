3aea68db9b760fc43f5376da0b597955b7cd59d3
Matan Kulikovsky <matan@soom.la>
2014-07-22 13:22:41 +0300

    copy customized marked-docpad-plugin during azure deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   var gruntConfig = require('./grunt-config.json');
   grunt.initConfig(gruntConfig);
-  grunt.registerTask('default', ['uglify', 'concat', 'cssmin']);
+  grunt.registerTask('default', ['uglify', 'concat', 'cssmin', 'copy']);
 };
