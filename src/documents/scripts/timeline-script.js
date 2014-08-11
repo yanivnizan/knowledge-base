642a032c51bdc531076c68c70388fd203ae5a72c
lenaspivak <lena@soom.la>
2014-08-11 14:02:34 +0300

    timeline

---
+++ src/documents/scripts/timeline-script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lena on 8/11/14.
+ */
+
+$("#blogpost-selected").click(function(){
+  $("#blogpost-post").hide();
+  $( this ).css( "background-image: url(/img/timeline-img/Highway-Clear.png);" );
+});
+
+$("#blogpost-selected").click(function(){
+  $("#blogpost-post").show();
+});
