e4e89dd13be6d28dca3225cc3a81c1d618d47023
Gur Dotan <gurdotan@gmail.com>
2014-11-23 14:27:45 +0200

    Test staging deployment

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -110,8 +110,10 @@ echo Installing Bower and Bower components...
 call !NPM_CMD! install -g bower
 IF !ERRORLEVEL! NEQ 0 goto error
 echo Bower installation successful
-echo "Deleting bower_components"
-call rd /s /q src\bower_components
+echo "Dir src"
+call dir src\bower_components
+echo "Dir src\bower_components"
+call dir src\bower_components
 call node_modules\.bin\bower install --verbose
 echo Bower components installation successful
 ::IF !ERRORLEVEL! NEQ 0 goto error
